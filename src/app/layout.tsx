a42058b0449e4bb0efc3f63a51b5a9f0fab81e16
HiaslHoam <matthiasheim24@live.de>
2026-02-22 18:50:53 +0100

    Yet another favicon fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,20 +18,20 @@ export async function generateMetadata(): Promise<Metadata> {
     description: config.tagline,
     icons: {
       icon: [
-        { url: '/favicon.ico' },
-        { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-        { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' }
+        { url: '/photos/favicon.ico' },
+        { url: '/photos/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+        { url: '/photos/favicon-32x32.png', sizes: '32x32', type: 'image/png' }
       ],
-      apple: [{ url: '/apple-touch-icon.png' }],
+      apple: [{ url: '/photos/apple-touch-icon.png' }],
       other: [
         {
           rel: 'android-chrome',
-          url: '/android-chrome-192x192.png',
+          url: '/photos/android-chrome-192x192.png',
           sizes: '192x192'
         },
         {
           rel: 'android-chrome',
-          url: '/android-chrome-512x512.png',
+          url: '/photos/android-chrome-512x512.png',
           sizes: '512x512'
         }
       ]
