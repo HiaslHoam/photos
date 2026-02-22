90dfe6603c41bdd4bb391140d82d315759e172c8
HiaslHoam <matthiasheim24@live.de>
2026-02-22 17:37:13 +0100

    Ready for build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/photos',
   images: {
+    unoptimized: true,
     domains: ['images.ctfassets.net', 'downloads.ctfassets.net']
   }
 };
